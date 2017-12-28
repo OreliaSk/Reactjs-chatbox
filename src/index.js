e0c1cc2455591e5c2f74277bc85f2e6eba65d428
Orelia Sokambi <orelia.sokambi@gmail.com>
2017-12-28 18:59:50 +0100

    add mock-up folder + class Connexion in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-//import './index.css';
+import { render } from 'react-dom';
+//import './style/SASS/import.css';
 
+class Connexion extends React.Component {
+	render() {
+		return (
+			<div className="bloc-connexion">
+				<form className="bloc-connexion__connexion">
+					<input placeholder="Enter your pseudo" required />
+					<button type="submit">Go chat !</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+render(<Connexion />, document.getElementById('root'));
